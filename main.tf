f66c70f99bcf33d76e12f5a806623987c62dff92
Gerald Mbevi <geraldbevi@gmail.com>
2026-02-17 12:37:01 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,1 +1,12 @@
+resource "aws_s3_bucket" "site" {
+  bucket = "my-secure-static-site-12345"
+}
 
+resource "aws_s3_bucket_public_access_block" "block" {
+  bucket = aws_s3_bucket.site.id
+
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
+}
